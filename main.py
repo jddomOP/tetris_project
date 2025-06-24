22ff25039cc539f1075a8947149d7736f7f12727
Jaydie <johndenieldomalaon@gmail.com>
2025-06-24 22:28:46 +0800

    adds quit event also imports sys

--- main.py
+++ main.py
@@ -9,4 +9,10 @@ pygame.display.set_caption("Python tetris")
 clock = pygame.time.Clock()
 
 while True:
-    
+    for event in pyfame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    pygame.display.update
+
