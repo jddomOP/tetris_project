5e572883c565dc7ff1f01750f6bc8fa04171eb89
Jaydie <johndenieldomalaon@gmail.com>
2025-06-24 23:15:40 +0800

    added necessary lines for the main python file

--- main.py
+++ main.py
@@ -21,6 +21,19 @@ while True:
         if event. type == pygame. QUIT:
             pygame. quit()
             sys. exit()
+        if event.type == pygame.KEYDOWN:
+            if game.game_over == True:
+                game.game_over = False
+                game.reset()
+            if event.key == pygame.K_LEFT and game.game_over == False:
+                game.move_left()
+            if event.key == pygame.K_RIGHT and game.game_over == False:
+                game.move_right()
+            if event.key == pygame.K_DOWN and game.game_over == False:
+                game.move_down()
+                game.update_score(0, 1)
+            if event.key == pygame.K_UP and game.game_over == False:
+                game.rotate()
 #Drawing
     screen.fill(dark_blue)
     game_grid.draw(screen)
