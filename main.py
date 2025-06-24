148ad48eda52a4e5cecba9919cfbe8fccb1d3463
Jaydie <johndenieldomalaon@gmail.com>
2025-06-24 23:06:33 +0800

    adding necessary codes

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ dark_blue = (44, 44, 127)
 
 screen = pygame.display.set_mode((300, 600))
 pygame. display.set_caption("Python Tetris")
+
 clock = pygame. time. Clock()
+
+game_grid = Grid()
+
+game_grid.grid[0][0] = 1
+game_grid.grid[3][5] = 4
+game_grid.grid[17][18] = 7
+
+game_grid.print_grid()
 
 while True:
     for event in pygame. event.get():
@@ -13,6 +22,8 @@ while True:
             pygame. quit()
             sys. exit()
 #Drawing
-screen.fill(dark_blue)
-pygame.display.update()
-clock.tick(60)
+    screen.fill(dark_blue)
+    game_grid.draw(screen)
+
+    pygame.display.update()
+    clock.tick(60)
